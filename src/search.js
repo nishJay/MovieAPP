f563975caa9cdc1cda194bb670fa81827c498557
R JAYANTH JADHAV <77871395+nishJay@users.noreply.github.com>
2023-06-28 22:18:02 +0530

    Add files via upload

---
+++ src/search.js
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+import MovieCard from "./moviecard";
+
+export default function SearchMovies() {
+  const [query, setQuery] = useState("");
+  const [movies, setMovies] = useState([]);
+
+  const searchMovies = async (e) => {
+    e.preventDefault();
+    console.log("Submitting");
+
+    const url = `https://api.themoviedb.org/3/search/movie?api_key=0f75b72d7ee3e188d51fa3ee75d1f465&language=en-US&query=${query}&page=1&include_adult=false`;
+
+    try {
+      const res = await fetch(url);
+      const data = await res.json();
+      setMovies(data.results);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  return (
+    <>
+      <form className="form" onSubmit={searchMovies}>
+        <label className="label" htmlFor="query">
+          Movie Name
+        </label>
+        <input
+          className="input"
+          type="text"
+          name="query"
+          placeholder="i.e. Jurassic Park"
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <button className="button" type="submit">
+          Search
+        </button>
+      </form>
+
+      <div className="card-list">
+        {movies &&
+          movies
+            .filter((movie) => movie.poster_path)
+            .map((movie) => <MovieCard movie={movie} key={movie.id} />)}
+      </div>
+    </>
+  );
+}
